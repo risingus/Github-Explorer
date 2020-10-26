955a80d927e8169ae34b341bc662e3bdd4a090f6
Gustavo Lima <gustavo.it@outlook.com>
2020-10-26 11:44:11 -0300

    Conectando aplicação com api do github

--- github-explorer/src/pages/Dashboard/index.tsx
+++ github-explorer/src/pages/Dashboard/index.tsx
@@ -1,49 +1,58 @@
-import React from 'react';
+import React, {useState, FormEvent} from 'react';
 import {Title, Form, Repositories} from './styles';
 import logoImg from '../../assets/logo.svg';
 import {FiChevronRight} from 'react-icons/fi';
+import api from '../../services/api';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string,
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
     <img src={logoImg} alt="Github Explorer"/>
     <Title>Explore respositórios no Github</Title>
 
-    <Form>
-      <input placeholder="Digite o nome do repositório"/>
+    <Form onSubmit={handleAddRepository}>
+      <input
+      value={newRepo}
+      onChange={(e) => setNewRepo(e.target.value)}
+      placeholder="Digite o nome do repositório"/>
       <button type="submit">Pesquisar</button>
     </Form>
 
     <Repositories>
-      <a href="teste">
-        <img src="https://avatars3.githubusercontent.com/u/42497575?s=460&u=661e39213d8f086557b48d1eae5e1a4441425929&v=4" alt="Gustavo Lima"/>
+     {repositories.map(repository => (
+      <a key={repository.full_name} href="teste">
+        <img src={repository.owner.avatar_url} alt={repository.owner.login} />
       <div>
-        <strong>risingus/Little-Invest</strong>
-        <p>A mobile first landing page for an investment site.</p>
+        <strong>{repository.full_name}</strong>
+        <p>{repository.description}</p>
       </div>
 
       <FiChevronRight size={20}></FiChevronRight>
       </a>
-
-      <a href="teste">
-        <img src="https://avatars3.githubusercontent.com/u/42497575?s=460&u=661e39213d8f086557b48d1eae5e1a4441425929&v=4" alt="Gustavo Lima"/>
-      <div>
-        <strong>risingus/Little-Invest</strong>
-        <p>A mobile first landing page for an investment site.</p>
-      </div>
-
-      <FiChevronRight size={20}></FiChevronRight>
-      </a>
-
-      <a href="teste">
-        <img src="https://avatars3.githubusercontent.com/u/42497575?s=460&u=661e39213d8f086557b48d1eae5e1a4441425929&v=4" alt="Gustavo Lima"/>
-      <div>
-        <strong>risingus/Little-Invest</strong>
-        <p>A mobile first landing page for an investment site.</p>
-      </div>
-
-      <FiChevronRight size={20}></FiChevronRight>
-      </a>
+     ))}
     </Repositories>
 
 
